76fb17e7f1d8410deab906f950ddb96895dfad49
Faez Shakil <faez.shakil@gmail.com>
2023-02-23 20:44:18 +0500

    added a flake encapsulating all libraries

--- flake.nix
+++ flake.nix
@@ -1,27 +1,45 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
 
-        pkgs = nixpkgs.legacyPackages.${system};
+      perSystem = { self', inputs', pkgs, system, lib, ... }:
+        let
+          toPackage = file: _: { name = file; value = {root = ./lib/services/${file}; }; };
 
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            haskellPackages.cabal-fmt
-            nixpkgs-fmt
-            ormolu
-            parallel
-            shellcheck
-            shfmt
-            stack
-          ];
+          isDir = _: x: x == "directory";
+          svcs = pkgs.lib.mapAttrs' toPackage
+            (lib.attrsets.filterAttrs isDir (builtins.readDir ./lib/services));
+          gogol = { gogol.root = ./lib/gogol; gogol-core.root = ./lib/gogol-core; };
+        in {
+          # Most simple configuration requires only:
+          # haskellProjects.default = { };
+
+          haskellProjects.default = {
+            # Haskell dependency overrides go here
+            # overrides = self: super: {
+            # };
+            # hlsCheck = false;
+            # hlintCheck = true;
+            packages = lib.attrsets.recursiveUpdate gogol svcs;
+            devShell.tools = hp: {
+                cabal-fmt = hp.cabal-fmt;
+                nixpkgs-fmt = pkgs.nixpkgs-fmt;
+                ormolu = pkgs.ormolu;
+                parallel = pkgs.parallel;
+                shellcheck = pkgs.shellcheck;
+                shfmt = pkgs.shfmt;
+                stack = pkgs.stack;
+            };
+          };
+          packages.default = self'.packages.gogol;
         };
-      });
+    };
 }
